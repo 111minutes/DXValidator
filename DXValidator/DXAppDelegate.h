c0680f2e0a24f8b8288d27fd88234f4176d2f7b4
Maxim <Letushov@Maxims-MacBook-Pro.local>
2012-10-23 16:11:13 +0300

    Empty project

---
+++ DXValidator/DXAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DXAppDelegate.h
+//  DXValidator
+//
+//  Created by Maxim on 10/23/12.
+//  Copyright (c) 2012 111Minutes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DXAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DXValidator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DXValidator
+//
+//  Created by Maxim on 10/23/12.
+//  Copyright (c) 2012 111Minutes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DXAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DXAppDelegate class]));
+    }
+}
